e7233027c7857c986b20a7252818c7427fee9241
Varadarajan <VVARADA1@ford.com>
2019-07-25 15:07:26 +0530

    Initial commit

---
+++ Manifest.bat
@@ -0,0 +1,5 @@
+gradlew cloudNativeDeveloperDistributedSystemDeployment \
+    -PregistrationServerUrl=https://registration-pal-vvarada1.apps.evans.pal.pivotal.io \
+    -PbacklogServerUrl=https://backlog-pal-vvarada1.apps.evans.pal.pivotal.io \
+    -PallocationsServerUrl=https://allocations-pal-vvarada1.apps.evans.pal.pivotal.io \
+    -PtimesheetsServerUrl=https://timesheets-pal-vvarada1.apps.evans.pal.pivotal.io
